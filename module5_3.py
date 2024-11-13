c41630f0fce911bd3d577548709fe80b0f1fa3bb
yarik333777 <132485222+yarik333777@users.noreply.github.com>
2024-11-13 14:08:49 +0300

    Add files via upload

---
+++ module5_3.py
@@ -0,0 +1,126 @@
+class house:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        print(f'This is house {self.name}, namber floors: {self.number_of_floors}')
+
+
+    def go_to(self, new_floors):
+        for i in range(1, new_floors):
+            if new_floors > self.number_of_floors or new_floors < 1:
+                print(f"There is no such floor: {new_floors}")
+                break
+            else:
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Names: {self.name}, quantity floors: {self.number_of_floors}  "
+
+    def __eq__(self, other):
+        return self.number_of_floors == other
+
+    def __add__(self, value):
+        return self.number_of_floors + value
+
+    def __lt__(self, other):
+        return self.number_of_floors < other
+
+    def __le__(self, other):
+        return self.number_of_floors <= other
+
+    def __gt__(self, other):
+        return self.number_of_floors > other
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other
+
+    def __ne__(self, other):
+        return self.number_of_floors != other
+h1 = house('ЖК Горский', 10)
+h2 = house('Домик в деревне', 20)
+h1.go_to(5)
+h2.go_to(10)
+print(len(h1))
+print(len(h2))
+print(str(h1))
+print(str(h2))
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 += 10  # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
+
+# Для решения этой задачи будем пользоваться решением к предыдущей задаче "Специальные методы класса".
+#
+# Необходимо дополнить класс House следующими специальными методами:
+# __eq__(self, other) - должен возвращать True, если количество этажей одинаковое у self и у other.
+# Методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=) должны присутствовать в классе и возвращать результаты сравнения по соответствующим операторам. Как и в методе __eq__ в сравнении участвует кол-во этажей.
+# __add__(self, value) - увеличивает кол-во этажей на переданное значение value, возвращает сам объект self.
+# __radd__(self, value), __iadd__(self, value) - работают так же как и __add__ (возвращают результат его вызова).
+# Остальные методы арифметических операторов, где self - x, other - y:
+#
+# Следует заметить, что other может быть не только числом, но и вообще любым объектом другого класса.
+# Для более точной логики работы методов __eq__, __add__  и других методов сравнения и арифметики перед выполняемыми действиями лучше убедиться в принадлежности к типу при помощи функции isinstance:
+# isinstance(other, int) - other указывает на объект типа int.
+# isinstance(other, House) - other указывает на объект типа House.
+#
+# Пример результата выполнения программы:
+# Пример выполняемого кода:
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# print(h1)
+# print(h2)
+#
+# print(h1 == h2) # __eq__
+#
+# h1 = h1 + 10 # __add__
+# print(h1)
+# print(h1 == h2)
+#
+# h1 += 10 # __iadd__
+# print(h1)
+#
+# h2 = 10 + h2 # __radd__
+# print(h2)
+#
+# print(h1 > h2) # __gt__
+# print(h1 >= h2) # __ge__
+# print(h1 < h2) # __lt__
+# print(h1 <= h2) # __le__
+# print(h1 != h2) # __ne__
+#
+# Вывод на консоль:
+# Название: ЖК Эльбрус, кол-во этажей: 10
+# Название: ЖК Акация, кол-во этажей: 20
+# False
+# Название: ЖК Эльбрус, кол-во этажей: 20
+# True
+# Название: ЖК Эльбрус, кол-во этажей: 30
+# Название: ЖК Акация, кол-во этажей: 30
+# False
+# True
+# False
+# True
+# False
+#
+# Примечания:
+# Методы __iadd__ и __radd__ не обязательно описывать заново, достаточно вернуть значение вызова __add__.
+# Более подробно о работе всех перечисленных методов можно прочитать здесь и здесь.
+#
